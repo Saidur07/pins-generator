ba1cb78d769b50f5306920cacfd0d7f25ef332c4
Saidur-07 <md.saidurrahman.bhola2007@gmail.com>
2022-02-13 13:49:19 +0600

    Scripts added

---
+++ script.js
@@ -0,0 +1,43 @@
+function getPin() {
+  let pin = Math.round(Math.random() * 9999);
+  let pinString = pin + "";
+  if (pinString.length == 4) {
+    return pin;
+  } else {
+    return getPin();
+  }
+}
+
+function generatePin() {
+  let displayPin = document.getElementById("display-pin");
+  displayPin.value = getPin();
+}
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  let number = event.target.innerText;
+  let calcInput = document.getElementById("typed-numbers");
+  if (isNaN(number)) {
+    if (number == "C") {
+      calcInput.value = "";
+    } else if (number == "Del") {
+      calcInput.value = calcInput.value.slice(0, -1);
+    }
+  } else {
+    let previousNumber = calcInput.value;
+    let newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+  }
+});
+
+function verifyPin() {
+  let pin = document.getElementById("display-pin").value;
+  let typedNumbers = document.getElementById("typed-numbers").value;
+  let failNotification = document.getElementById("notify-fail");
+  let successNotification = document.getElementById("notify-success");
+  if (pin == typedNumbers) {
+    successNotification.style.display = "block";
+    failNotification.style.display = "none";
+  } else {
+    failNotification.style.display = "block";
+    successNotification.style.display = "none";
+  }
+}
